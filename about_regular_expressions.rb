296fb2174d8acf2bc096164762415c54b05a4ec4
Richard Shin <skunkworks@gmail.com>
2013-11-01 18:56:42 -0700

    Finished regular expressions

--- about_regular_expressions.rb
+++ about_regular_expressions.rb
@@ -134,54 +134,91 @@ class AboutRegularExpressions < Neo::Koan
     assert_equal '42', "2 lines\nnum 42"[/\d+$/]
   end
 
-  # \b matches to the beginning or end of a word. TODO: more clarification!
+  # \b matches to the beginning or end of a word, where a word is defined as
+  # a group of one or more consecutive alphanumeric + underscore characters.
+  # It does not match with any whitespace characters that may exist before the
+  # end of that word
   def test_slash_b_anchors_to_a_word_boundary
     assert_equal 'vines', "bovine vines"[/\bvine./]
   end
 
   # ------------------------------------------------------------------
 
+  # You can specify regex match groups using parentheses. Doing so provides not
+  # only the ability to specify precisely what group of characters to match
+  # for, but also allows us to recall the group matches (as seen in the next
+  # few examples)
   def test_parentheses_group_contents
     assert_equal 'hahaha', "ahahaha"[/(ha)+/]
   end
 
   # ------------------------------------------------------------------
 
+  # A number can be supplied to act as a match capture group index to have it
+  # return the specified match group.
+  #
+  # Note that this must be a *group*, i.e. a match within a set of parentheses.
+  # For example, 'Richard'[/Richard/, 1] would return nil, since there is no
+  # match group in the regex pattern /Richard/. On the other hand, something
+  # like 'Richard'[/(Richard)/, 1] would return 'Richard'
   def test_parentheses_also_capture_matched_content_by_number
-    assert_equal __, "Gray, James"[/(\w+), (\w+)/, 1]
-    assert_equal __, "Gray, James"[/(\w+), (\w+)/, 2]
+    assert_equal 'Gray', "Gray, James"[/(\w+), (\w+)/, 1]
+    assert_equal 'James', "Gray, James"[/(\w+), (\w+)/, 2]
   end
 
+  # I was vaguely familiar with the $1/$2/$N method of accessing matched groups
+  # from a regex, but I didn't realize that they are automatically provided for
+  # you after performing a match.
   def test_variables_can_also_be_used_to_access_captures
-    assert_equal __, "Name:  Gray, James"[/(\w+), (\w+)/]
-    assert_equal __, $1
-    assert_equal __, $2
+    assert_equal 'Gray, James', "Name:  Gray, James"[/(\w+), (\w+)/]
+    assert_equal 'Gray', $1
+    assert_equal 'James', $2
   end
 
   # ------------------------------------------------------------------
 
+  # The first example is pretty straightforward to understand: our new regex
+  # pattern matches either (James, Dana, or Summer) Gray.
+  #
+  # The second example does the same, but also asks for the matched content
+  # group 1.
+  #
+  # The third example of course is a match miss, which would return nil, and
+  # trying to get back match group 1 would still be nil.
   def test_a_vertical_pipe_means_or
     grays = /(James|Dana|Summer) Gray/
-    assert_equal __, "James Gray"[grays]
-    assert_equal __, "Summer Gray"[grays, 1]
-    assert_equal __, "Jim Gray"[grays, 1]
+    assert_equal 'James Gray', "James Gray"[grays]
+    assert_equal 'Summer', "Summer Gray"[grays, 1]
+    assert_equal nil, "Jim Gray"[grays, 1]
   end
 
   # THINK ABOUT IT:
   #
   # Explain the difference between a character class ([...]) and alternation (|).
-
+  #
+  # Answer: A character class specifies a range of valid matching characters
+  # for a given match, whereas the alternation allows you to specify multiple
+  # groups to match
   # ------------------------------------------------------------------
 
+  # scan returns an array of *all* matches in a string for a given pattern.
+  # Previous examples only returned the first match found in the string.
+  #
+  # With scan, if the pattern contains no groups as in the example below, the
+  # resulting array will contain the matched strings. However, if the pattern
+  # contains groups, each result in the array is itself an array containing
+  # one entry per group in the pattern.
   def test_scan_is_like_find_all
-    assert_equal __, "one two-three".scan(/\w+/)
+    assert_equal ['one', 'two', 'three'], "one two-three".scan(/\w+/)
   end
 
+  # Note that sub is only a one-time find-and-replace operation. gsub is a
+  # find-and-replace-all operation!
   def test_sub_is_like_find_and_replace
-    assert_equal __, "one two-three".sub(/(t\w*)/) { $1[0, 1] }
+    assert_equal 'one t-three', "one two-three".sub(/(t\w*)/) { $1[0, 1] }
   end
 
   def test_gsub_is_like_find_and_replace_all
-    assert_equal __, "one two-three".gsub(/(t\w*)/) { $1[0, 1] }
+    assert_equal 'one t-t', "one two-three".gsub(/(t\w*)/) { $1[0, 1] }
   end
 end
