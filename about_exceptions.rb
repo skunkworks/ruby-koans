ea6ed640678ef12ce295eb33484c193d1e05a9f5
Richard Shin <skunkworks@gmail.com>
2013-11-02 13:59:05 -0700

    Finished about_exceptions

--- about_exceptions.rb
+++ about_exceptions.rb
@@ -5,13 +5,21 @@ class AboutExceptions < Neo::Koan
   class MySpecialError < RuntimeError
   end
 
+  # Ancestors of a class are listed in ascending order in the hierarchy, i.e.
+  # [RuntimeError, StandardError, Exception, Object, ...]
   def test_exceptions_inherit_from_Exception
-    assert_equal __, MySpecialError.ancestors[1]
-    assert_equal __, MySpecialError.ancestors[2]
-    assert_equal __, MySpecialError.ancestors[3]
-    assert_equal __, MySpecialError.ancestors[4]
+    assert_equal RuntimeError, MySpecialError.ancestors[1]
+    assert_equal StandardError, MySpecialError.ancestors[2]
+    assert_equal Exception, MySpecialError.ancestors[3]
+    assert_equal Object, MySpecialError.ancestors[4]
   end
 
+  # fail() throws a RuntimeError with an optional message argument.
+  #
+  # Note that rescue StandardError => ex handles StandardError and any
+  # descendent classes. Also, note the syntax to capture the exception:
+  #
+  # rescue StandardError => ex
   def test_rescue_clause
     result = nil
     begin
@@ -20,30 +28,33 @@ class AboutExceptions < Neo::Koan
       result = :exception_handled
     end
 
-    assert_equal __, result
+    assert_equal :exception_handled, result
 
-    assert_equal __, ex.is_a?(StandardError), "Should be a Standard Error"
-    assert_equal __, ex.is_a?(RuntimeError),  "Should be a Runtime Error"
+    assert_equal true, ex.is_a?(StandardError), "Should be a Standard Error"
+    assert_equal true, ex.is_a?(RuntimeError),  "Should be a Runtime Error"
 
     assert RuntimeError.ancestors.include?(StandardError),
       "RuntimeError is a subclass of StandardError"
 
-    assert_equal __, ex.message
+    assert_equal 'Oops', ex.message
   end
 
   def test_raising_a_particular_error
     result = nil
     begin
       # 'raise' and 'fail' are synonyms
+      # Didn't know this!!!
       raise MySpecialError, "My Message"
     rescue MySpecialError => ex
       result = :exception_handled
     end
 
-    assert_equal __, result
-    assert_equal __, ex.message
+    assert_equal :exception_handled, result
+    assert_equal 'My Message', ex.message
   end
 
+  # Note that calling fail/raise without specifying a type will default to
+  # throwing a StandardError exception
   def test_ensure_clause
     result = nil
     begin
@@ -54,13 +65,14 @@ class AboutExceptions < Neo::Koan
       result = :always_run
     end
 
-    assert_equal __, result
+    # begin/rescue/ensure maps to C#'s try/catch/finally blocks
+    assert_equal :always_run, result
   end
 
   # Sometimes, we must know about the unknown
   def test_asserting_an_error_is_raised
     # A do-end is a block, a topic to explore more later
-    assert_raise(___) do
+    assert_raise(MySpecialError) do
       raise MySpecialError.new("New instances can be raised directly.")
     end
   end
