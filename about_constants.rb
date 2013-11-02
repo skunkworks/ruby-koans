f33f54baa2e87be92d1198f3b84e8a8cddd3f46a
Richard Shin <skunkworks@gmail.com>
2013-11-02 13:58:33 -0700

    Finished about_constants

--- about_constants.rb
+++ about_constants.rb
@@ -4,19 +4,34 @@ C = "top level"
 
 class AboutConstants < Neo::Koan
 
+  # Don't forget that any capitalized variable is a constant!
   C = "nested"
 
   def test_nested_constants_may_also_be_referenced_with_relative_paths
-    assert_equal __, C
+    assert_equal 'nested', C
   end
 
+  # I've wondered what the double-colon notation meant. I've seen it when
+  # referencing other modules, e.g. Nokogiri::XML, or Neo::Koan. The formal
+  # definition is:
+  #
+  # The :: is a unary operator that allows: constants, instance methods and
+  # class methods defined within a class or module, to be accessed from
+  # anywhere outside the class or module.
+  #
+  # In plain English, this means that it's a namespace operator that allows you
+  # to specify exactly which constant or method in which class/module you're
+  # trying to access. In C#, you can accomplish this with namespace dot
+  # notation.
+  #
+  # Note that a naked :: without anything preceding it is the global scope.
   def test_top_level_constants_are_referenced_by_double_colons
-    assert_equal __, ::C
+    assert_equal 'top level', ::C
   end
 
   def test_nested_constants_are_referenced_by_their_complete_path
-    assert_equal __, AboutConstants::C
-    assert_equal __, ::AboutConstants::C
+    assert_equal 'nested', AboutConstants::C
+    assert_equal 'nested', ::AboutConstants::C
   end
 
   # ------------------------------------------------------------------
@@ -34,8 +49,9 @@ class AboutConstants < Neo::Koan
     end
   end
 
+  # Constants are inherited by nested classes
   def test_nested_classes_inherit_constants_from_enclosing_classes
-    assert_equal __, Animal::NestedAnimal.new.legs_in_nested_animal
+    assert_equal 4, Animal::NestedAnimal.new.legs_in_nested_animal
   end
 
   # ------------------------------------------------------------------
@@ -46,8 +62,9 @@ class AboutConstants < Neo::Koan
     end
   end
 
+  # Similarly, subclasses will inherit constants from parent classes
   def test_subclasses_inherit_constants_from_parent_classes
-    assert_equal __, Reptile.new.legs_in_reptile
+    assert_equal 4, Reptile.new.legs_in_reptile
   end
 
   # ------------------------------------------------------------------
@@ -63,12 +80,14 @@ class AboutConstants < Neo::Koan
   end
 
   def test_who_wins_with_both_nested_and_inherited_constants
-    assert_equal __, MyAnimals::Bird.new.legs_in_bird
+    assert_equal 2, MyAnimals::Bird.new.legs_in_bird
   end
 
   # QUESTION: Which has precedence: The constant in the lexical scope,
   # or the constant from the inheritance hierarchy?
-
+  #
+  # Answer: the constant in the lexical scope holds precedence over any
+  # inherited constants.
   # ------------------------------------------------------------------
 
   class MyAnimals::Oyster < Animal
@@ -78,10 +97,19 @@ class AboutConstants < Neo::Koan
   end
 
   def test_who_wins_with_explicit_scoping_on_class_definition
-    assert_equal __, MyAnimals::Oyster.new.legs_in_oyster
+    assert_equal 4, MyAnimals::Oyster.new.legs_in_oyster
   end
 
   # QUESTION: Now which has precedence: The constant in the lexical
   # scope, or the constant from the inheritance hierarchy?  Why is it
   # different than the previous answer?
+  #
+  # Answer: The constant from the inheritance hierarchy now has precedence.
+  # This is because the class definition of the Oyster class is not technically
+  # in the lexical scope of MyAnimals, it's only in the same namespace based
+  # on its class definition.
+  # 
+  # If you print out the nesting by doing p Module.nesting in each class, you'd
+  # see that although Oyster is in the MyAnimals namespace, it doesn't inherit
+  # anything defined in MyAnimals.
 end
