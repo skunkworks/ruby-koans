474a62c2d38934563b72da0b10de92e00283a963
Richard Shin <skunkworks@gmail.com>
2013-11-15 16:19:23 -0800

    Finished about_inheritance

--- about_inheritance.rb
+++ about_inheritance.rb
@@ -24,23 +24,23 @@ class AboutInheritance < Neo::Koan
   end
 
   def test_subclasses_have_the_parent_as_an_ancestor
-    assert_equal __, Chihuahua.ancestors.include?(Dog)
+    assert_equal true, Chihuahua.ancestors.include?(Dog)
   end
 
   def test_all_classes_ultimately_inherit_from_object
-    assert_equal __, Chihuahua.ancestors.include?(Object)
+    assert_equal true, Chihuahua.ancestors.include?(Object)
   end
 
   def test_subclasses_inherit_behavior_from_parent_class
     chico = Chihuahua.new("Chico")
-    assert_equal __, chico.name
+    assert_equal 'Chico', chico.name
   end
 
   def test_subclasses_add_new_behavior
     chico = Chihuahua.new("Chico")
-    assert_equal __, chico.wag
+    assert_equal :happy, chico.wag
 
-    assert_raise(___) do
+    assert_raise(NoMethodError) do
       fido = Dog.new("Fido")
       fido.wag
     end
@@ -48,10 +48,10 @@ class AboutInheritance < Neo::Koan
 
   def test_subclasses_can_modify_existing_behavior
     chico = Chihuahua.new("Chico")
-    assert_equal __, chico.bark
+    assert_equal 'yip', chico.bark
 
     fido = Dog.new("Fido")
-    assert_equal __, fido.bark
+    assert_equal 'WOOF', fido.bark
   end
 
   # ------------------------------------------------------------------
@@ -62,9 +62,10 @@ class AboutInheritance < Neo::Koan
     end
   end
 
+  # Didn't realize that calling the parent was as simple as "super"
   def test_subclasses_can_invoke_parent_behavior_via_super
     ralph = BullDog.new("Ralph")
-    assert_equal __, ralph.bark
+    assert_equal 'WOOF, GROWL', ralph.bark
   end
 
   # ------------------------------------------------------------------
@@ -75,9 +76,15 @@ class AboutInheritance < Neo::Koan
     end
   end
 
+  # Not surprising that this wouldn't work. super is a call to the method in
+  # the superclass and other ancestors.
+  #
+  # Note that super by itself forwards arguments from the invoking method.
+  # To call it with no arguments, use super(). To give it new arguments,
+  # specify them manually (e.g. super(a, b, c)).
   def test_super_does_not_work_cross_method
     george = GreatDane.new("George")
-    assert_raise(___) do
+    assert_raise(NoMethodError) do
       george.growl
     end
   end
