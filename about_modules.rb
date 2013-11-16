ed2c606b1d018cacc601a1684e268bf9e10f085c
Richard Shin <skunkworks@gmail.com>
2013-11-15 16:23:42 -0800

    Finished about_modules

--- about_modules.rb
+++ about_modules.rb
@@ -1,5 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
+# I have some familiarity with modules from CS169.1x.
+
 class AboutModules < Neo::Koan
   module Nameable
     def set_name(new_name)
@@ -12,13 +14,14 @@ class AboutModules < Neo::Koan
   end
 
   def test_cant_instantiate_modules
-    assert_raise(___) do
+    assert_raise(NoMethodError) do
       Nameable.new
     end
   end
 
   # ------------------------------------------------------------------
 
+  # Don't forget that modules are "include"d.
   class Dog
     include Nameable
 
@@ -39,7 +42,7 @@ class AboutModules < Neo::Koan
 
   def test_normal_methods_are_available_in_the_object
     fido = Dog.new
-    assert_equal __, fido.bark
+    assert_equal 'WOOF', fido.bark
   end
 
   def test_module_methods_are_also_available_in_the_object
@@ -51,13 +54,14 @@ class AboutModules < Neo::Koan
 
   def test_module_methods_can_affect_instance_variables_in_the_object
     fido = Dog.new
-    assert_equal __, fido.name
+    assert_equal 'Fido', fido.name
     fido.set_name("Rover")
-    assert_equal __, fido.name
+    assert_equal 'Rover', fido.name
   end
 
+  # Makes sense -- modules have lower priority scope-wise.
   def test_classes_can_override_module_methods
     fido = Dog.new
-    assert_equal __, fido.here
+    assert_equal :in_object, fido.here
   end
 end
